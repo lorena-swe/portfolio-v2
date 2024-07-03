dbaf6b29a329f9fdba9733d0bf2c5de3306a881a
Lorena <lorenazotaj10@gmail.com>
2024-07-03 11:25:43 +0200

    Customized Experiences tabs content

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import "./ExperienceSection.css";
+import {
+  Avatar,
+  Box,
+  Flex,
+  Heading,
+  HoverCard,
+  Link,
+  Text,
+} from "@radix-ui/themes";
 
 function ExperienceSection() {
   const [activeTab, setActiveTab] = useState("tab1");
@@ -7,43 +16,147 @@ function ExperienceSection() {
   return (
     <div
       id="experience"
-      className="section h-screen w-full py-10 px-8 md:px-16 flex flex-col justify-center"
+      className="section h-screen w-full py-10 px-8 md:px-16 flex flex-col border border-sky-500 justify-start"
     >
-      <div className="tabs-container">
-        <div className="tabs-list">
+      <div className="tabs-container border border-sky-500">
+        <div className="tabs-list flex flex-row sm:flex-col min-w-48">
           <button
-            className={`tabs-trigger ${activeTab === "tab1" ? "active" : ""}`}
+            className={`tabs-trigger w-full ${
+              activeTab === "tab1" ? "active" : ""
+            }`}
             onClick={() => setActiveTab("tab1")}
           >
-            Tab 1
+            Deloitte
           </button>
           <button
-            className={`tabs-trigger ${activeTab === "tab2" ? "active" : ""}`}
+            className={`tabs-trigger w-full  ${
+              activeTab === "tab2" ? "active" : ""
+            }`}
             onClick={() => setActiveTab("tab2")}
           >
-            Tab 2
+            Salesforce Industries
           </button>
           <button
-            className={`tabs-trigger ${activeTab === "tab3" ? "active" : ""}`}
+            className={`tabs-trigger w-full  ${
+              activeTab === "tab3" ? "active" : ""
+            }`}
             onClick={() => setActiveTab("tab3")}
           >
-            Tab 3
+            Amazon
           </button>
         </div>
-        <div className="tabs-content-container">
+        <div className="tabs-content-container max-w-full lg:max-w-xl">
           {activeTab === "tab1" && (
             <div className="tabs-content">
-              <p>Content for Tab 1</p>
+              <div className="exp-title">
+                Frontend Engineer{" "}
+                <strong>
+                  <HoverCard.Root>
+                    <HoverCard.Trigger>
+                      <Link href="https://twitter.com/radix_ui" target="_blank">
+                        @ Deloitte
+                      </Link>
+                    </HoverCard.Trigger>
+                    <HoverCard.Content maxWidth="300px">
+                      <Flex gap="4">
+                        <Avatar
+                          size="3"
+                          fallback="R"
+                          radius="full"
+                          src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
+                        />
+                        <Box>
+                          <Heading size="3" as="h3">
+                            Radix
+                          </Heading>
+                          <Text as="div" size="2">
+                            React components, icons, and colors for building
+                            high-quality, accessible UI.
+                          </Text>
+                        </Box>
+                      </Flex>
+                    </HoverCard.Content>
+                  </HoverCard.Root>
+                </strong>
+              </div>
+              <p className="exp-date">December 2022 - Present</p>
+              <ul className="exp-bullets">
+                <li>
+                  Managed frontend development for an e-commerce website
+                  migration project, developing 20 landing pages and over 70
+                  frontend components using JavaScript, HTML, CSS, React, and
+                  Jest.
+                </li>
+                <li>
+                  Refined early design drafts by integrating advanced UX
+                  principles, leading to a 30% improvement in user retention and
+                  a significant 20% increase in client endorsement.
+                </li>
+                <li>
+                  Optimized a streamlined customer service feature, enabling
+                  visualization of unregistered accounts and bulk re-invitation
+                  emails, improving efficiency by 40% and reducing response
+                  times by 25%.
+                </li>
+                <li>
+                  Enhanced backend calls efficiency in the checkout process,
+                  resulting in 85% decrease in page load times, and created
+                  intuitive drop-down menus with advanced search
+                  functionalities, elevating user experience and increasing
+                  engagement by 15%.
+                </li>
+                <li>
+                  Conducted unit testing with Java across the entire codebase,
+                  achieving an average coverage of 87%, ensuring robust
+                  functionality and stability.
+                </li>
+              </ul>
             </div>
           )}
           {activeTab === "tab2" && (
             <div className="tabs-content">
-              <p>Content for Tab 2</p>
+              <div>
+                Salesforce Developer <strong>@ Salesforce Industries</strong>
+              </div>
+              <p>April 2002 - July 2022</p>
+              <ul>
+                <li>
+                  Configured, deployed, and maintained Salesforce Components and
+                  metadata across three projects.
+                </li>
+                <li>
+                  Executed rigorous testing and debugging of API requests using
+                  Postman; reduced error rate by 10% by executing unit and
+                  system tests, error handling, and bug fixes.
+                </li>
+                <li>
+                  Coordinated with a 12-member cross-functional team, utilizing
+                  daily stand-ups to sync development efforts, streamline
+                  processes, increase project efficiency by 15%, and meet all
+                  deadlines.
+                </li>
+              </ul>
             </div>
           )}
           {activeTab === "tab3" && (
             <div className="tabs-content">
-              <p>Content for Tab 3</p>
+              <div>
+                IT Support Engineer Intern <strong>@ Amazon</strong>
+              </div>
+              <p>March 2021 - May 2021</p>
+              <ul>
+                <li>
+                  Developed and designed front-end interfaces for internal tools
+                  and websites
+                </li>
+                <li>
+                  Contributed to implementation discussions on daily meetings
+                </li>
+                <li>
+                  Regularly interacted with management and staff to provide
+                  analytical and technical assistance
+                </li>
+              </ul>
             </div>
           )}
         </div>
